7cecbdd3b5863ac8a0baf65c0ab95969bb505d0c
Made Aryadinata <made.aryadinata2@btpn.com>
2020-10-14 02:50:57 +0700

    .rc/nodejs.fish: move npmrc base64

--- .rc/nodejs.fish
+++ .rc/nodejs.fish
@@ -1,12 +1,12 @@
 if test -e ~/.npmrc
   set NPM_REGISTRY (cat ~/.npmrc | grep '^registry' | sed -e 's/registry=//' | sed 's/.$//')
   set NPM_TOKEN (cat ~/.npmrc | grep '_authToken' | sed -e 's/.*_authToken\=//')
+  set NPMRC (base64 ~/.npmrc)
 end
 
 alias nt="npm run test"
 alias ntc="npm run test:coverage"
 
-set NPMRC (base64 ~/.npmrc)
 
 if type -q fnm
   eval (fnm env --multi)
