bb56e58edefbcf949fdc197393a635eea4d4779f
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2020-10-14 02:44:01 +0700

    .rc/nodejs.fish check .npmrc before exporting variables, add --multi on fnm env

--- .rc/nodejs.fish
+++ .rc/nodejs.fish
@@ -1,5 +1,7 @@
-set NPM_REGISTRY (cat ~/.npmrc | grep '^registry' | sed -e 's/registry=//' | sed 's/.$//')
-set NPM_TOKEN (cat ~/.npmrc | grep '_authToken' | sed -e 's/.*_authToken\=//')
+if test -e ~/.npmrc
+  set NPM_REGISTRY (cat ~/.npmrc | grep '^registry' | sed -e 's/registry=//' | sed 's/.$//')
+  set NPM_TOKEN (cat ~/.npmrc | grep '_authToken' | sed -e 's/.*_authToken\=//')
+end
 
 alias nt="npm run test"
 alias ntc="npm run test:coverage"
@@ -7,5 +9,5 @@ alias ntc="npm run test:coverage"
 set NPMRC (base64 ~/.npmrc)
 
 if type -q fnm
-  eval (fnm env)
+  eval (fnm env --multi)
 end
