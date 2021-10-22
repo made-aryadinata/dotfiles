58d8f42e8a6ec20e3659a9729a75c9f096bcd6c4
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2021-10-22 13:49:10 +0700

    .rc/nodejs.fish: fix NPMRC for linux and update fnm

--- .rc/nodejs.fish
+++ .rc/nodejs.fish
@@ -1,7 +1,12 @@
 if test -e ~/.npmrc
-  set NPM_REGISTRY (cat ~/.npmrc | grep '^registry' | sed -e 's/registry=//' | sed 's/.$//')
-  set NPM_TOKEN (cat ~/.npmrc | grep '_authToken' | sed -e 's/.*_authToken\=//')
-  set NPMRC (base64 ~/.npmrc)
+  set -gx NPM_REGISTRY (cat ~/.npmrc | grep '^registry' | sed -e 's/registry=//' | sed 's/.$//')
+  set -gx NPM_TOKEN (grep '_authToken' ~/.npmrc | head -1 | sed -e 's/.*_authToken\=//')
+
+  if test (uname -s) = "Darwin"
+    set -gx NPMRC (base64 ~/.npmrc)
+  else if test (uname -s) = "Linux"
+    set -gx NPMRC (base64 -w 0 ~/.npmrc)
+  end
 end
 
 alias nt="npm run test"
@@ -9,5 +14,5 @@ alias ntc="npm run test:coverage"
 
 
 if type -q fnm
-  eval (fnm env --multi)
+  fnm env | source
 end
