12fa7e7c3633fd6dd38b86da02b38e7be451072c
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2021-12-14 09:41:02 +0700

    .rc/themes.fish: use terminal emulator colors as fish colors

--- .rc/main.fish
+++ .rc/main.fish
@@ -5,6 +5,7 @@ source $rc_dir/ide.fish
 source $rc_dir/nodejs.fish
 source $rc_dir/profile.fish
 source $rc_dir/ssh.fish
+source $rc_dir/themes.fish
 
 if test -e $rc_dir/work/work.fish
   source $rc_dir/work/work.fish
