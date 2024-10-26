c69e25405018a9699bef185d77568dbc6893c1e7
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2024-10-26 19:32:14 +0700

    .rc/init.fish: add eza, fzf, zoxide, use brew, .rc/main.fish: use brew on linux

--- .rc/init.fish
+++ .rc/init.fish
@@ -3,17 +3,34 @@ if not type -q fisher
   echo 'Installing fisher...'
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 end
+
+if not type -q brew
+  echo 'Installing brew...'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if test (uname -s) = "Darwin"
+    eval (/opt/homebrew/bin/brew shellenv)
+  else if test (uname -s) = "Linux"
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  end
+end
+
 fisher install Gazorby/fish-abbreviation-tips
 fisher install jethrokuan/z
 fisher install PatrickF1/fzf.fish
 
-if not type -q fnm
-  echo 'Installing fnm...'
-  curl -fsSL https://fnm.vercel.app/install | bash
-  set -U fish_user_paths $HOME/.fnm $fish_user_paths
+function install_if_not_exists -a pkg
+  if not command -v $pkg
+    echo 'Installing '$pkg'...'
+
+    brew install $pkg
+  end
 end
 
-if not type -q starship
-  echo 'Installing starship...'
-  curl -fsSL https://starship.rs/install.sh | bash
-end
+install_if_not_exists fnm
+install_if_not_exists starship
+install_if_not_exists eza
+install_if_not_exists fzf
+install_if_not_exists zoxide
+
+fish_add_path $HOME/.fnm
