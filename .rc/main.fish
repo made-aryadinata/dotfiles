bf09f953a618901752b2fe10128fef046a9c75ca
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2024-10-26 19:32:14 +0700

    .rc/init.fish: add eza, fzf, zoxide, use brew, .rc/main.fish: use brew on linux

--- .rc/main.fish
+++ .rc/main.fish
@@ -7,6 +7,8 @@ end
 
 if test (uname -s) = "Darwin"
   eval (/opt/homebrew/bin/brew shellenv)
+else if test (uname -s) = "Linux"
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
 if type -q zoxide
