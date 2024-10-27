aff57a3621f9beca242185392628a39378cdb309
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2024-10-27 23:42:01 +0700

    .dotfiles: use bash for installations

--- .dotfiles/init.fish
+++ .dotfiles/init.fish
@@ -1,28 +1,14 @@
-#!/usr/bin/env fish
-function install_if_not_exists -a pkg cli
-  if test -z "$cli"
-    set cli $pkg
-  end
-
-  if not type -q $cli
-    echo "Installing $pkg..."
-    brew install $pkg
-  end
-end
-
-install_if_not_exists fisher
-install_if_not_exists fnm
-install_if_not_exists starship
-install_if_not_exists eza
-install_if_not_exists fzf
-install_if_not_exists zoxide
-install_if_not_exists bitwarden-cli bw
-install_if_not_exists nnn
-install_if_not_exists fd
-install_if_not_exists macchina
-
 set --universal fish_greeting (macchina)
 fish_add_path $HOME/.fnm
 
-fisher install Gazorby/fish-abbreviation-tips
-fisher install PatrickF1/fzf.fish
+function install_plugin_if_not_exists -a plugin
+  if not fisher list | grep -q "^$plugin\$"
+    echo "Installing $plugin..."
+    fisher install $plugin
+  else
+    echo "$plugin is already installed."
+  end
+end
+
+install_plugin_if_not_exists gazorby/fish-abbreviation-tips
+install_plugin_if_not_exists patrickf1/fzf.fish
--- .dotfiles/init.sh
+++ .dotfiles/init.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+install_if_not_exists() {
+  local pkg=$1
+
+  if ! brew list --formula | grep -q "^${pkg}\$"; then
+    echo "Installing $pkg..."
+    brew install "$pkg"
+  else
+    echo "$pkg is already installed."
+  fi
+}
+
 if ! command -v brew &> /dev/null; then
   echo 'Installing brew...'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -10,9 +21,16 @@ if ! command -v brew &> /dev/null; then
   fi
 fi
 
-if ! command -v fish &> /dev/null; then
-  echo 'Installing fish...'
-  brew install fish
-fi
+install_if_not_exists fish
+install_if_not_exists fisher
+install_if_not_exists fnm
+install_if_not_exists starship
+install_if_not_exists eza
+install_if_not_exists fzf
+install_if_not_exists zoxide
+install_if_not_exists bitwarden-cli bw
+install_if_not_exists nnn
+install_if_not_exists fd
+install_if_not_exists macchina
 
 fish ~/.dotfiles/init.fish
